81dde97f2682c77075ec1e118d7d2ceb32600daf
Sean Alberts <sean.alberts@dish.com>
2017-06-16 12:25:04 -0600

    Completed cachematrix.R assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+# cachematrix.R
+# Coursera R Programming - Programming Assignment 2: Lexical scoping
+# contains two functions:
+#
+# makeCacheMatrix
+# cacheSolve 
 
-## Write a short comment describing this function
-
+#makeCacheMatrix
+# Given a matrix, defines four functions to operate on that matrix
+# set uses the <<-- assignment operator to cache the input matrix
+# get returns the cached value of the matrix
+# setinvert uses the <<-- assignment to cache the invert of the input matrix
+# getinvert returns the cached invert matrix if already setinvert, otherwise returns null
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  
+  get<-function()x
+  setinvert<-function(solve) m <<-solve
+  getinvert<-function()m
+  list(set=set,get=get,setinvert=setinvert,getinvert=getinvert)
+  
 
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve
+# Given a matrix, checks to see if the invert has already been setinvert with makeCacheMatrix.
+# If so, the cached invert is returned.  
+# Otherwise, the invert of the cached matrix is found and cached using the setinvert function of makeCacheMatrix, then returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  m<-x$getinvert()
+  if(!is.null(m)){
+    message("Getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data)
+  x$setinvert(m)
+  m
+
 }
